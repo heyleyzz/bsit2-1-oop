b69fc315a46b9ebd5dad0a64ba06dbeef4a06d9e
Liz Heart B. Silubrico <lhsilubrico1893@liceo.edu.ph>
2025-08-18 09:42:09 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Create GradeCalculator object
         GradeCalculator calculator = new GradeCalculator();
 
-        // Sample student
         String studentName = "John Smith";
         double average = calculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
         String letterGrade = calculator.getLetterGrade(average);
 
-        // Display using both overloaded methods
         System.out.println("first displayResult");
         calculator.displayResult(studentName, average);
 
